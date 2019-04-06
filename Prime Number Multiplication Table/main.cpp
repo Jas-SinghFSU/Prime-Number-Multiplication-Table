6dbc06f1e4a8073be071beb3f44fbe619fa529c4
Jas <Jas@maysas-MacBook-Air.local>
2019-04-06 13:04:13 -0700

    Initial Commit

---
+++ Prime Number Multiplication Table/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Prime Number Multiplication Table
+//
+//  Created by Jas Singh on 4/6/19.
+//  Copyright © 2019 Funding Circle Code Challenge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
